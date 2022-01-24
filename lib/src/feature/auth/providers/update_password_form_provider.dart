5824da34767857d8a7c2826ed9909bcc9f686daf
Tyler Savery <tyler@theyoungastronauts.com>
2022-01-24 17:05:46 -0500

    refactor update password to provider

---
+++ lib/src/feature/auth/providers/update_password_form_provider.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_skeleton/src/core/app/loading_provider.dart';
+import 'package:flutter_skeleton/src/core/app/singletons.dart';
+import 'package:flutter_skeleton/src/core/app_router.gr.dart';
+import 'package:flutter_skeleton/src/core/dialogs/dialogs.dart';
+import 'package:flutter_skeleton/src/feature/auth/services/auth_service.dart';
+import 'package:flutter_skeleton/src/utils/toast.dart';
+import 'package:flutter_skeleton/src/utils/validation.dart';
+
+class UpdatePasswordFormProvider {
+  Reader read;
+  UpdatePasswordFormProvider(this.read);
+
+  Future<void> showDialog(String token) async {
+    final password = await PromptModal.show(
+      title: "Update Password",
+      validator: formValidatorPassword,
+      labelText: "New Password",
+      obscureText: true,
+      confirmText: "Update",
+    );
+
+    if (password == null) {
+      return;
+    }
+
+    read(loadingProvider.notifier).start();
+
+    final success = await AuthService().completePasswordReset(
+      password: password,
+      token: token,
+    );
+    if (success) {
+      read(loadingProvider.notifier).complete();
+      singleton<AppRouter>().push(const LoginScreenRoute());
+      Toast.message("Password updated successfully");
+      return;
+    } else {
+      Toast.error();
+    }
+    read(loadingProvider.notifier).complete();
+  }
+}
+
+final updatePasswordFormProvider = Provider(
+  (ref) => UpdatePasswordFormProvider(ref.read),
+);
--- lib/src/feature/auth/screens/update_password_screen.dart
+++ lib/src/feature/auth/screens/update_password_screen.dart
@@ -1,13 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_skeleton/src/core/app/loading_provider.dart';
-import 'package:flutter_skeleton/src/core/app/singletons.dart';
-import 'package:flutter_skeleton/src/core/app_router.gr.dart';
-import 'package:flutter_skeleton/src/core/dialogs/dialogs.dart';
 import 'package:flutter_skeleton/src/core/base_screen.dart';
-import 'package:flutter_skeleton/src/feature/auth/services/auth_service.dart';
-import 'package:flutter_skeleton/src/utils/toast.dart';
-import 'package:flutter_skeleton/src/utils/validation.dart';
+import 'package:flutter_skeleton/src/feature/auth/providers/update_password_form_provider.dart';
 
 class UpdatePasswordScreen extends BaseStatefulScreen {
   final String token;
@@ -30,33 +24,7 @@ class _UpdatePasswordScreenState extends BaseScreenState<UpdatePasswordScreen> {
   }
 
   Future<void> _showDialog() async {
-    final password = await PromptModal.show(
-      title: "Update Password",
-      validator: formValidatorPassword,
-      labelText: "New Password",
-      obscureText: true,
-      confirmText: "Update",
-    );
-
-    if (password == null) {
-      return;
-    }
-
-    ref.read(loadingProvider.notifier).start();
-
-    final success = await AuthService().completePasswordReset(
-      password: password,
-      token: widget.token,
-    );
-    if (success) {
-      ref.read(loadingProvider.notifier).complete();
-      singleton<AppRouter>().push(const LoginScreenRoute());
-      Toast.message("Password updated successfully");
-      return;
-    } else {
-      Toast.error();
-    }
-    ref.read(loadingProvider.notifier).complete();
+    ref.read(updatePasswordFormProvider).showDialog(widget.token);
   }
 
   @override
